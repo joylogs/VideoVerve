dfe083c3fe3813187adaeaf5e2b04b0eb5e8cbb2
Joy <joy.logs@gmail.com>
2024-02-28 23:55:21 +0530

    added APICall handling URLRequest, Error Added WebRepository to handle published status of requests Added Cancelbag to handle cancellation of Combine Tasks

---
+++ VideoVerve/Utilities/CancelBag.swift
@@ -0,0 +1,33 @@
+//
+//  CancelBag.swift
+//  VideoVerve
+//
+//  Created by Joy Banerjee on 28/02/24.
+//
+
+import Combine
+
+final class CancelBag {
+    fileprivate(set) var subscriptions = Set<AnyCancellable>()
+    private let equalToAny: Bool
+    
+    init(equalToAny: Bool = false) {
+        self.equalToAny = equalToAny
+    }
+    
+    func cancel() {
+        subscriptions.removeAll()
+    }
+    
+    func isEqual(to other: CancelBag) -> Bool {
+        return other === self || other.equalToAny || self.equalToAny
+    }
+}
+
+extension AnyCancellable {
+    
+    func store(in cancelBag: CancelBag) {
+        cancelBag.subscriptions.insert(self)
+    }
+}
+
