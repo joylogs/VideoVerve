685169e6252996f7f71220238873377c7ef851c8
Joy <joy.logs@gmail.com>
2024-03-11 13:27:27 +0530

    adding a placeholder thumbnail image

--- VideoVerve/UI/Components/FeedItem.swift
+++ VideoVerve/UI/Components/FeedItem.swift
@@ -6,41 +6,65 @@
 //
 
 import SwiftUI
+import Combine
 import AVKit
 
 struct FeedItem: View {
-    
     let feed: Feed
     
+    //    @State private var feed: Loadable<Feed>
+    
+    //    init(feed: Feed) {
+    //        self.feed = feed
+    //    }
+    
     var body: some View {
-            VStack(alignment: .leading) {
-                HStack {
-                    CircleImage(image: Image("turtlerock"))
+        
+        VStack(alignment: .center) {
+            HStack {
+                CircleImage(image: Image("turtlerock"))
                     .scaleEffect(1.0 / 3.0)
-
-                    Text("\(feed.username)")
-                        .font(.callout)
-                        .bold()
-                    Spacer()
-                    
-                    HStack {
-                        Text("\(feed.likes) Likes")
-                            .font(.caption)
-                    }
-                }
                 
-                HStack {
-                    Spacer()
-                    
-                    VideoPlayer(player: player(feed: feed))
-                        .frame(width: 320,
-                               height: 180, alignment: .top)
-                    
-                    Spacer()
+                Text("\(feed.username)")
+                    .font(.callout)
+                    .bold()
+                Spacer()
+                
+                Text("\(feed.likes) Likes")
+            }
+            
+            VideoPlayer(player: player(feed: feed))
+                .frame(width: 320, height: 180, alignment: .top)
+                .overlay {
+                    Image("turtlerock")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .ignoresSafeArea()
                 }
-
-                Divider()
+            
+            Divider()
         }
+    }
+    
+    func generateThumbImage(url: URL) -> UIImage {
+        
+        var asset: AVAsset = AVAsset(url: url)
+        var assetImgGenerate: AVAssetImageGenerator = AVAssetImageGenerator(asset: asset)
+        
+        assetImgGenerate.appliesPreferredTrackTransform = true
+        
+        var time: CMTime = CMTimeMake(value: 1, timescale: 30)
+        var image: CGImage
+        do {
+            image = try assetImgGenerate.copyCGImage(at: time, actualTime: nil)
+        } catch {
+            image = UIImage(named: "turtlerock")!.cgImage!
+            print(error.localizedDescription)
+        }
+        
+        var frameImage: UIImage = UIImage(cgImage: image)
+        
+        return frameImage
     }
     
     func player(feed: Feed) -> AVPlayer {
